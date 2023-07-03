0c91acc6ade70f84821d61f63009f18f1a3dedcc
aMilles <32961942+aMilles@users.noreply.github.com>
2023-07-03 16:11:13 +0200

    revised folder correction

--- R/checkFoldernames.R
+++ R/checkFoldernames.R
@@ -1,41 +1,18 @@
-checkFoldernames <- function(){
+checkFoldernames <- function(importTables_list){
 
-  if(!exists("ereignis_import") & !exists("daten_import")){
-    stop(print("Benötigte Daten 'ereignis_import' und/oder 'daten_import' fehlen. Führe die Funktion import() aus."))
+  falsche_ordnernamen_df <- unique(importTables_list$daten[standort_id != standort_id_folder, .(standort_id,standort_id_folder),])
+
+  if(nrow(falsche_ordnernamen_df) > 0){
+    print("Die Ordnernamen folgender Standorte werden korrigiert: ")
+    print(falsche_ordnernamen_df$standort_id_folder)
   }
 
-  #wrong folder names --> "standort_id_folder" is the correct one
-  falsche_ordnernamen <- unique(ereignis_import[standort_id != standort_id_folder,
-                                                .(standort_id,standort_id_folder),])
-
-  #df
-  falsche_ordnernamen_all <- falsche_ordnernamen
-  colnames(falsche_ordnernamen_all) <- c("alt","korrigiert")
-  assign("falsche_ordnernamen_all",falsche_ordnernamen_all, envir =  .GlobalEnv)
-
-  if (nrow(falsche_ordnernamen) >0) {
-    print("Folgende Standorte haben eine falsche Ordnerbenennung, die nicht mit dem Standortnamen übereinstimmt: ")
-    print(falsche_ordnernamen)
-
-    #name "standort_id" and "ordner" like "standort_id_folder"
-    for (i in 1:nrow(falsche_ordnernamen)) {
-      #daten
-      daten_import[standort_id_folder == falsche_ordnernamen$standort_id_folder[i],
-                     ':='(ordner = falsche_ordnernamen$standort_id_folder[i],
-                          standort_id = falsche_ordnernamen$standort_id_folder[i])
-                  ,]
-      #ereignis
-      ereignis_import[standort_id_folder == falsche_ordnernamen$standort_id_folder[i],
-                      ':='(ordner = falsche_ordnernamen$standort_id_folder[i],
-                          standort_id = falsche_ordnernamen$standort_id_folder[i])
-                  ,]
-    }
-
-    print("Die Ordnernamen folgender Standorte wurden korrigiert: ")
-    print(falsche_ordnernamen)
-
-  } else {
-    print('Alle Ordnernamen stimmen überein!')
-  }
+  c(lapply(importTables_list, function(data){
+    data[standort_id_folder != standort_id,
+         ':='(ordner = falsche_ordnernamen$standort_id_folder[i],
+              standort_id = falsche_ordnernamen$standort_id_folder[i])
+         ,]
+  }),
+  falsche_ordnernamen_df)
 
 }
--- R/checkSpeciesCorr.R
+++ R/checkSpeciesCorr.R
@@ -2,14 +2,16 @@ checkSpeciesCorr <- function(importTables_list, falsche_tierarten_df){
 
   ft_df <- data.table::as.data.table(falsche_tierarten_df)
 
-  lapply(importTables_list, function(dt){
-    dt <- data.table::merge.data.table(dt, ft_df, by.y = "alt", by.x = "tierart_1", all.x = TRUE)
+  lapply(importTables_list, function(dt_in){
+    dt <- data.table::merge.data.table(dt_in, ft_df, by.y = "alt", by.x = "tierart_1", all.x = TRUE)
     dt[, tierart_1 := ifelse(is.na(korrigiert), tierart_1, korrigiert)]
     dt[, c("korrigiert", "anzahl"):= NULL]
 
     dt <- data.table::merge.data.table(dt, ft_df, by.y = "alt", by.x = "tierart_2", all.x = TRUE)
     dt[, tierart_2 := ifelse(is.na(korrigiert), tierart_2, korrigiert)]
     dt[, c("korrigiert", "anzahl"):= NULL]
+
+    setcolorder(dt, names(dt_in))
 
     return(dt)
   })
