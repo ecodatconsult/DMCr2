84020f70159c9bdcea954aab66d9fd36ad87eb14
aMilles <32961942+aMilles@users.noreply.github.com>
2023-07-10 17:47:59 +0200

    revised function to copy pictures from central directory

--- R/extractPictures.R
+++ R/extractPictures.R
@@ -1,67 +1,52 @@
-extractPictures <- function(save_path, scale = TRUE, size = "x480", tags){
+#' Ruft Bilder aus dem Ordnerverzeichnis anhand der Datenbankinfo ab
+#'
+#' @param bilder_table data.table, Datentabelle mit Information zu den Pfaden zu den Bildern (i.d.R. erstellt mit downloadDB("bilder", "alle Arten", "alle Projekte"))
+#' @param save_path character, Pfad zu dem Speicherort für die Bilder
+#' @param scale character, gibt die Bildhöhe an. "x480" ändert die Bildhöhe auf 480 Pixel, der aspect ratio wird beibehalten
+#' @param tags character vector, gibt die tags an, die in den Bildern eingetragen werden sollen
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
 
-  if(exists("ergebnisse", envir = .GlobalEnv)){
+extractPictures <- function(bilder_table, save_path, scale = TRUE, size = "x480", tags){
 
-    require(RPostgreSQL)
-    require(DBI)
-    require(stringi)
-    require(magick)
-    require(dplyr)
+    con <- dbConnection()
 
-    dbConnection(type = "read")
+    df_standorte_bilderordner <- RPostgreSQL::dbGetQuery(con,"SELECT DISTINCT(bilderordner) FROM fotofallen.fotofallen_standorte")$bilderordner
 
-    # data frame
-    df_standorte <- dbGetQuery(con,"SELECT * FROM fotofallen.fotofallen_standorte")
+    RPostgreSQL::dbDisconnect(con)
 
-    dbDisconnect(con)
+    # construct path 2 images, detect missing path info and accessbility issues
+    bilder_table <-
+    bilder_table %>%
+      dplyr::select(ordner, bildname) %>%
+      dplyr::mutate(pfad_ordner = sapply(ordner, function(x) df_standorte_bilderordner[grep(x, df_standorte_bilderordner)])) %>%
+      dplyr::mutate(pfad_bild = paste0(pfad_ordner, .Platform$file.sep, bildname)) %>%
+      dplyr::mutate(pfad_gegeben = !any(is.na(c(pfad_ordner, ordner, bildname)))) %>%
+      dplyr::mutate(pfad_abrufbar = file.exists(pfad_bild)) %>%
+      dplyr::as_tibble()
 
-    # pfade
-    ergebnisse_mit_pfaden <- ergebnisse
+    # read and process images based on the input
+    apply(bilder_table %>%
+            filter(pfad_gegeben & pfad_abrufbar),
+          1,
+          function(x){
+            img <- magick::image_read(x$pfad_bild)
 
-    pfade_nicht_vorhanden <-c()
-    for(i in 1:nrow(ergebnisse_mit_pfaden)){
-      ordner <- ergebnisse_mit_pfaden$ordner[i]
-      bildname <- ergebnisse_mit_pfaden$bildname[i]
-      ordner_bildname <- paste(ordner,bildname, sep = "/")
-      pfad_ordner <- df_standorte$bilderordner[grep(ordner, df_standorte$bilderordner)]
-      pfad_bild <- paste(pfad_ordner, bildname, sep = "/")
-      if(length(pfad_bild[grep(":/",pfad_bild)])>0){
-        ergebnisse_mit_pfaden$pfad_bild[i] <- pfad_bild
-      }else{
-        pfade_nicht_vorhanden <- append(pfade_nicht_vorhanden,ordner_bildname)
-        ergebnisse_mit_pfaden$pfad_bild[i] <- NA
-      }
-    }
-    assign("pfade_nicht_vorhanden_df",pfade_nicht_vorhanden, envir = .GlobalEnv)
-    rm(pfade_nicht_vorhanden)
+            img %>%
+              magick::image_scale(ifelse(scale, size, paste0("x", magick::image_info(img)$height))) %>%
+              magick::image_annotate(paste(paste(tags,x[,tags],sep = ": "),collapse = "\n"),
+                                     size = 20,
+                                     color = "black",
+                                     boxcolor = "white",
+                                     location = "+10+10") %>%
+              magick::image_mosaic() %>%
+              magick::image_write(paste0(save_path,.Platform$file.sep,x$ordner,"-",x$bildname))
+          })
 
-    # bilder
-    pfade_ohne_zugriff <- c()
-    for(i in 1:nrow(ergebnisse_mit_pfaden)){
-      tryCatch(
-        bild <- image_read(ergebnisse_mit_pfaden$pfad_bild[i])
-      , error=function(e) {})
-      if(exists("bild")){
-          #scale it
-          if(scale == TRUE){
-            bild <- image_scale(bild, size)
-          }
-          #annotate
-          bild <- image_annotate(bild, paste(paste(tags,ergebnisse_mit_pfaden[i,tags],sep = ": "),collapse = ", "),
-                                 size = 20,
-                                 color = "black",
-                                 boxcolor = "white",
-                                 location = "+10+10")
-          #save
-          image_write(image_mosaic(bild),path = paste(save_path,"/",ergebnisse_mit_pfaden$ordner[i],"-",ergebnisse_mit_pfaden$bildname[i], sep = ""))
-          rm(bild)
-      }else{
-          pfade_ohne_zugriff <- append(pfade_ohne_zugriff, ergebnisse_mit_pfaden$pfad_bild[i])
-      }
-    }
-    pfade_ohne_zugriff <- pfade_ohne_zugriff[!is.na(pfade_ohne_zugriff)]
-    assign("pfade_ohne_zugriff_df",pfade_ohne_zugriff, envir = .GlobalEnv)
-    rm(pfade_ohne_zugriff)
-
-  }
+    # return missing and inaccesible paths
+    return(list(pfade_ohne_zugriff_df = bilder_table$pfad_bild[!bilder_table$pfad_abrufbar],
+                pfade_nicht_vorhanden_df = bilder_table$ordner[!bilder_table$pfad_gegeben]))
 }
