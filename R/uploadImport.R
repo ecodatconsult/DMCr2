da696071cce3e4d32f42a365f43225280ba0a287
aMilles <32961942+aMilles@users.noreply.github.com>
2023-07-16 13:53:17 +0200

    - import Functions from data.table - updated uploadImport.R to facilitate removal of duplicated bilderdaten and ereignisse in R before uploading to the DB. The new parameter schema also allows to upload ereignisse and bilderdaten directly to fotofallen.bilder or fotofallen.ereignisse

--- R/uploadImport.R
+++ R/uploadImport.R
@@ -1,46 +1,63 @@
 #' Datentabellen (daten und ereignis) der Session werden in die FVA-Fotofallendatenbank hochgeladen (PostgreSQL)
 #'
 #' @param importTables_list list, Liste mit Daten und Ereignissen als data.tables nach Durchführung der Korrekturfunktionen
+#' @param schema character, Schema in das der Upload durchgeführt werden soll, Duplikate werden nicht in das gewählte Schema importiert
 #' @return Nothing
 #' @import data.table
 #' @export
 #'
 # Fotofallen_Standorte
-uploadImport <- function(importTables_list){
+uploadImport <- function(importTables_list, schema = "import"){
 
   con <- dbConnection()
-  #TODO remove folder and standort_id_folder earlier?!
-  daten_import <- importTables_list$daten
-  daten_import[, c("folder", "standort_id_folder") := NULL,]
+  # upload bilder
+  importTables_list <- processImportTables(importTables(file_path = "inst/test_data/", data = c("daten", "ereignis")))
 
-  RPostgreSQL::dbWriteTable(con, c("import", "bilder"),
-               daten_import, row.names = FALSE, append = TRUE)
+  locs2upload <- unique(importTables_list$daten$ordner)
+  locs_in_db <- RPostgreSQL::dbGetQuery(con, glue::glue_sql("select distinct ordner from ", schema, ".bilder where ordner in ({upload_locs*})", upload_locs = locs2upload, .con = con)) #TODO make sure that ordner is sufficient id!!
 
-  # lösche Daten, die doppelt importiert wurden
-  #TODO: check this SQL-code
-  RPostgreSQL::dbSendQuery(con, "DELETE FROM import.bilder
-  WHERE bild_id IN (SELECT bild_id
-  FROM (SELECT bild_id,
-  ROW_NUMBER() OVER (partition BY ordner, bildname, ts ORDER BY bild_id DESC) AS rnum
-  FROM import.bilder) t
-                           WHERE t.rnum > 1);")
+  if(any(!locs2upload %in% locs_in_db)){
+    daten_import <- importTables_list$daten[!ordner %in% locs_in_db,]
 
+    #TODO remove folder and standort_id_folder earlier?!
+    daten_import[, c("folder", "standort_id_folder") := NULL,]
 
-  ereignis_import <- importTables_list$ereignis
+    RPostgreSQL::dbWriteTable(con,
+                              c(schema, "bilder"),
+                              daten_import,
+                              row.names = FALSE,
+                              append = TRUE)
 
-  #TODO remove pfad, folder and standort_id_folder earlier?!
-  ereignis_import[, c("pfad", "folder", "standort_id_folder") := NULL,]
+    fun_message1 <- paste0(nrow(daten_import), " Zeilen wurden in ", schema, ".bilder hochgeladen!")
+  }else{
+    fun_message1 <- paste0("Bereits alle Standorte in ", schema, ".bilder vorhanden. Es ist kein Upload erfolgt!")
+  }
 
-  RPostgreSQL::dbWriteTable(con, c("import", "ereignisse"),
-                            ereignis_import, row.names = FALSE, append = TRUE)
+  # upload ereignisse
+  locs2upload <- unique(importTables_list$ereignis$ordner)
+  locs_in_db <- RPostgreSQL::dbGetQuery(con, glue::glue_sql("select distinct ordner from ", schema, ".ereignisse where ordner in ({upload_locs*})", upload_locs = locs2upload, .con = con))  #TODO make sure that ordner is sufficient id!!
 
-   # lösche Daten, die doppelt importiert wurden
-  #TODO: check this SQL-code
-  RPostgreSQL::dbSendQuery(con, "DELETE FROM import.ereignisse
-  WHERE ereignis_id IN (SELECT ereignis_id
-  FROM (SELECT ereignis_id, ROW_NUMBER() OVER (partition BY ordner, s_ereignis_id, ts ORDER BY ereignis_id DESC) AS rnum
-  FROM import.ereignisse) t
-                           WHERE t.rnum > 1);")
+  if(any(!locs2upload %in% locs_in_db)){
+    ereignis_import <- importTables_list$ereignis[!ordner %in% locs_in_db,]
+
+    #TODO remove folder and standort_id_folder earlier?!
+    ereignis_import[, c("folder", "standort_id_folder") := NULL,]
+
+    RPostgreSQL::dbWriteTable(con,
+                              c(schema, "ereignisse"),
+                              ereignis_import,
+                              row.names = FALSE,
+                              append = TRUE)
+
+    fun_message2 <- paste0(nrow(ereignis_import), " Zeilen wurden in ", schema, ".ereignis hochgeladen!")
+  }else{
+    fun_message2 <- paste0("Bereits alle Standorte in ", schema, ".ereignis vorhanden. Es ist kein Upload erfolgt!")
+  }
 
   DBI::dbDisconnect(con)
+
+  return(data.frame(
+    bilder_upload_message = fun_message1,
+    ereignisse_upload_message = fun_message2
+  ))
 }
